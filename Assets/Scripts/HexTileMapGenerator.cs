b39e1f5108a31472291702fa39c0c4183a2aba3c
HorizonSB <sem.budaev@mail.ru>
2022-09-30 17:15:16 +0300

    Добавил ячейки с ресурсами и сделал гибкую настройку генерации

--- Assets/Scripts/HexTileMapGenerator.cs
+++ Assets/Scripts/HexTileMapGenerator.cs
@@ -1,18 +1,51 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class HexTileMapGenerator : MonoBehaviour
 {
-    [SerializeField]private GameObject hexTilePrefab;
+    [SerializeField]private GameObject[] hexTilePrefab;
+
+
+    List<GameObject> cells = new List<GameObject>();
 
     [SerializeField]int mapWidth = 25;
     [SerializeField]int mapHeight = 12;
+
+    [Header("Resources cells")]
+    public int cellResourcesAmmount;
+    [SerializeField] private GameObject[] resourcesCells;
+
+    [Header("Enemy cells")]
 
     float tileXOffset = 1.736f;
     float tileZOffset = 1.506f;
 
     void Start()
     {
+        CreateListOfCells();
         CreateHexTileMap();
+    }
+
+    void CreateListOfCells()
+    {
+        //Ammount of all cells
+        int ammountOfGrassCells = (mapHeight + 1) * (mapWidth + 1);
+        //Add portal cell
+        cells.Add(hexTilePrefab[0]);
+        //Add resources cells
+        for(int i =0; i<= cellResourcesAmmount-1; i++)
+        {
+            for(int r = 0; r <= resourcesCells.Length; r++)
+            {
+                if(r<resourcesCells.Length) cells.Add(resourcesCells[r]);
+            }
+        }
+        //Add grass cells
+        for(int i =0; i <= ammountOfGrassCells; i++)
+        {
+            cells.Add(hexTilePrefab[1]);
+        }
     }
 
     void CreateHexTileMap()
@@ -21,9 +54,12 @@ public class HexTileMapGenerator : MonoBehaviour
         {
             for(int z = 0; z <= mapHeight; z++)
             {
-                GameObject TempGO = Instantiate(hexTilePrefab);
+                int random = Random.Range(0, cells.Count);
 
-                if(z % 2 == 0)
+                GameObject TempGO = Instantiate(cells[random]);
+                cells.RemoveAt(random);
+
+                if (z % 2 == 0)
                 {
                     TempGO.transform.position = new Vector3(x * tileXOffset, 0, z * tileZOffset);
                 }
