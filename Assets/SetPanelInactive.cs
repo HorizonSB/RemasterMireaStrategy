89a5e8086c048ca431138c3af80fb7756794d869
HorizonSB <sem.budaev@mail.ru>
2022-11-09 12:36:00 +0300

    EndBuild

---
+++ Assets/SetPanelInactive.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPanelInactive : MonoBehaviour
+{
+    public void SetInactive() => gameObject.SetActive(false);
+}
